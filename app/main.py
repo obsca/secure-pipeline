a1664afc1dc92ac64bda8fc6d896f0492b04d98f
Parallels <parallels@debian-gnu-linux-12-11.localdomain>
2026-03-29 17:59:38 +0300

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import sqlite3
+
+app = FastAPI()
+
+@app.get("/users")
+def get_user(name: str):
+    conn = sqlite3.connect("test.db")
+    cursor = conn.cursor()
+
+    # ❌ SQL injection для демонстрации
+    query = f"SELECT * FROM users WHERE name = '{name}'"
+    cursor.execute(query)
+
+    return {"data": cursor.fetchall()}
